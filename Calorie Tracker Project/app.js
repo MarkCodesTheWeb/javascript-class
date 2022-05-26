aeadfa9b1940bc95d8685dde586eb3bb3307eded
Gamerilla <markastrus@gmail.com>
2022-05-26 11:54:14 -0400

    update calorie tracker

--- Calorie Tracker Project/app.js
+++ Calorie Tracker Project/app.js
@@ -1,9 +1,9 @@
 // Storage Controller
 
 // Item Controller
-const ItemCtrl = (function(){
+const ItemCtrl = (function () {
   //Item constructor
-  const Item = function(id, name, calories){
+  const Item = function (id, name, calories) {
     this.id = id;
     this.name = name;
     this.calories = calories;
@@ -11,35 +11,76 @@ const ItemCtrl = (function(){
 
   //Data Structure / State
   const data = {
-    items: [
-      {id: 0, name: 'Steak Dinner', calories: 1200},
-      {id: 1, name: 'Cookie', calories: 400},
-      {id: 2, name: 'Eggs', calories: 300}
+    items: [{
+        id: 0,
+        name: 'Steak Dinner',
+        calories: 1200
+      },
+      {
+        id: 1,
+        name: 'Cookie',
+        calories: 400
+      },
+      {
+        id: 2,
+        name: 'Eggs',
+        calories: 300
+      }
     ],
     currentItem: null,
     totalCalories: 0
   }
+
+
+  //Public Methods
   return {
-    logData: function(){
+    getItems: function () {
+      return data.items;
+    },
+    logData: function () {
       return data;
     }
   }
 })();
 
 // UI Controller
-const UICtrl = (function(){
+const UICtrl = (function () {
+  const UISelectors = {
+    itemList: '#item-list'
+  }
+  //public methods
   return {
-    
+    populateItemList: function(items){
+      let html = '';
+
+      items.forEach(function(item) {
+        html += `
+        <li class="collection-item" id="item-${item.id}">
+    <strong>${item.name} </strong> <em>${item.calories}</em>
+    <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+  </li>
+        `;
+        
+      });
+
+     // Insert list items in list
+     document.querySelector(UISelectors.itemList).innerHTML = html; 
+    } 
+
   }
 
 })();
 //App Controller
-const App = (function(ItemCtrl, UICtrl){
-    return {
-      init: function(){
-        console.log('Initializing App...')
-      }
+const App = (function (ItemCtrl, UICtrl) {
+  return {
+    init: function () {
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+      //populate list with items
+      UICtrl.populateItemList(items);
+      
     }
+  }
 })(ItemCtrl, UICtrl);
 
 //Initialize App
